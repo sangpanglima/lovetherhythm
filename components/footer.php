70b1f80136fd1af38dca275d684e0a23a994c040
sangpanglima <155632877+sangpanglima@users.noreply.github.com>
2024-01-04 18:27:30 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <!-- <div class="box">
+         <h3>tautan langsung</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div> -->
+
+      <div class="box">
+         <h3>tautan tambahan</h3>
+         <a href="admin/admin_login.php"> <i class="fas fa-angle-right"></i> Login as Admin</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Keranjang</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="#"><i class="fas fa-phone"></i> +62 215 819 0805 </a>
+         <a href="#"><i class="fas fa-phone"></i> +62 215 821 5723</a>
+         <a href="#"><i class="fas fa-envelope"></i>LoveTheRhythm@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Pekanbaru, Riau, Indonesia</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/lovetherhythm1/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>LoveTheRhythm</span> | all rights reserved!</div>
+
+</footer>
